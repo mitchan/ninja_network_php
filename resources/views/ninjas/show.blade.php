85de47f343a079a1d079ff5f44ea876059a89d78
Francesco Casetta <francescocasetta@gmail.com>
2025-01-31 08:26:43 +0100

    lesson 03

---
+++ resources/views/ninjas/show.blade.php
@@ -0,0 +1,12 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Ninja Network</title>
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <h2>Ninja ID - {{ $id }}</h2>
+    </body>
+</html>
+
