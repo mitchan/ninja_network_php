8ea2f34a06bd98bac568acba91864446b1caaea4
Francesco Casetta <francescocasetta@gmail.com>
2025-02-01 09:29:00 +0100

    lesson 04

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -7,15 +7,18 @@
     </head>
     <body>
          <h2>Currently Available Ninjas</h2>
-         <p>{{ $greeting }}</p>
+
+         @if($greeting == "hello")
+            <p>Hi</p>
+         @endif
 
          <ul>
-             <li>
-                <a href="/ninjas/{{ $ninjas[0]["id"] }}">{{ $ninjas[0]["name"] }}</a>
-             </li>
-             <li>
-                <a href="/ninjas/{{ $ninjas[1]["id"] }}">{{ $ninjas[1]["name"] }}</a>
-             </li>
+            @foreach($ninjas as $ninja)
+                <li>
+                    <p>{{ $ninja['name'] }}</p>
+                    <a href="/ninjas/{{ $ninja['id'] }}">View detail</a>
+                </li>
+            @endforeach
          </ul>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/ninjas', function () {
         ["name" => "luigi", "skill" => 45, "id" => "2"],
     ];
 
-    return view('ninjas.index', ["greeting" => "hello", "ninjas" => $ninjas]);
+    return view('ninjas.index', ["greeting" => "hi", "ninjas" => $ninjas]);
 });
 
 Route::get('/ninjas/{id}', function ($id) {
